2b0974a729781eac92c41f13f34489a0ee0c4a7d
kmb0000 <anthony.denquin97@gmail.com>
2026-03-04 15:56:17 +0100

    refactor(types): complete RiotParticipant typing

--- src/domain/match/match.types.ts
+++ src/domain/match/match.types.ts
@@ -17,17 +17,55 @@ export interface AggregatedStats {
 
 export interface RiotParticipant {
   puuid: string;
+
   kills: number;
   deaths: number;
   assists: number;
+
   totalMinionsKilled: number;
   neutralMinionsKilled: number;
+
   win: boolean;
+
+  championName: string;
+  champLevel: number;
+
+  item0: number;
+  item1: number;
+  item2: number;
+  item3: number;
+  item4: number;
+  item5: number;
+  item6: number;
 }
 
 export interface RiotMatch {
   info: {
     gameDuration: number;
+    gameEndTimestamp: number;
     participants: RiotParticipant[];
   };
+}
+
+export interface MatchItem {
+  item0: number;
+  item1: number;
+  item2: number;
+  item3: number;
+  item4: number;
+  item5: number;
+  item6: number;
+}
+
+export interface PlayerMatch {
+  championName: string;
+  championLevel: number;
+  kills: number;
+  deaths: number;
+  assists: number;
+  totalCs: number;
+  durationMinutes: number;
+  win: boolean;
+  items: MatchItem;
+  gameEndTimestamp: number;
 }
